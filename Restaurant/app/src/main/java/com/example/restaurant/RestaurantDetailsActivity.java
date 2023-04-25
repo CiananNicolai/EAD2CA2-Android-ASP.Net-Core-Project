a2738f12118572c2c92afc05a76544df70abd865
Elijel <x00161872@mytudublin.ie>
2023-04-25 17:04:14 +0100

    changed Layout on Homepage, fixed bugs

--- Restaurant/app/src/main/java/com/example/restaurant/RestaurantDetailsActivity.java
+++ Restaurant/app/src/main/java/com/example/restaurant/RestaurantDetailsActivity.java
@@ -77,6 +77,7 @@ public class RestaurantDetailsActivity extends AppCompatActivity {
                             String phone = response.getString("phone");
                             String description = response.getString("description");
                             int stars = (int) response.getInt("stars");
+                            String pictureUrl = response.getString("pictureUrl");
 
                             // display the restaurant data on the activity
                             nameTextView.setText(name);
@@ -85,6 +86,7 @@ public class RestaurantDetailsActivity extends AppCompatActivity {
                             phoneTextView.setText(phone);
                             descriptionTextView.setText(description);
                             ratingBar.setRating(stars);
+                            Picasso.get().load(pictureUrl).into(restaurantImageView);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -128,6 +130,7 @@ public class RestaurantDetailsActivity extends AppCompatActivity {
                             phoneTextView.setText(phone);
                             descriptionTextView.setText(description);
                             ratingBar.setRating(stars);
+                            Picasso.get().load(pictureUrl).into(restaurantImageView);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
